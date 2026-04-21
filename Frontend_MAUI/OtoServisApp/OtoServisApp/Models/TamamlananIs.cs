700c2231b2be866bc797f9a485a707dfcc23cd0c
Çağatay ERDOĞDU <cagatayerdogdu@hotmail.com>
2026-04-21 11:08:37 +0300

    hata

--- Frontend_MAUI/OtoServisApp/OtoServisApp/Models/TamamlananIs.cs
+++ Frontend_MAUI/OtoServisApp/OtoServisApp/Models/TamamlananIs.cs
@@ -19,4 +19,5 @@ public class TamamlananIs
 
     public string TamResimUrl => $"{ApiConfig.BaseUrl.TrimEnd('/')}/{ResimUrl?.TrimStart('/').Replace("\\", "/")}";
 
+    public ImageSource? ResimSource { get; set; }
 }
